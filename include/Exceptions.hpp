cd2f650e9d23e96fc33ad1b30f6dd62d087d63a7
zeviberlin <zeviberlin@gmail.com>
2025-08-24 20:29:54 -0400

    Added FailedSocketError exception class

--- include/Exceptions.hpp
+++ include/Exceptions.hpp
@@ -19,18 +19,35 @@
 
 namespace phi {
 
-class InvalidIPv4AddressException : public std::exception {
-	private:
-		std::string message_;
+class InvalidIPv4AddressError : public std::exception {
+  private:
+    std::string message_;
 
-	public:
-		InvalidAddressException(const struct sockaddr_in& addr) : message_("Invalid IPv4 Address `" + addr.sin_addr  + ":" + addr.sin_port) {}
+  public:
+    InvalidAddressException(const struct sockaddr_in& addr)
+        : message_("Invalid IPv4 address `" + addr.sin_addr + ":" +
+                   addr.sin_port) {
+    }
 
-		virtual const std::string what() const noexcept override {
-			return message_;
-		}
+    virtual const std::string what() const noexcept override {
+      return message_;
+    }
 }
 
-} // namespace phi
+class FailedSocketError : public std::runtime_error {
+  private:
+    std::string message_;
+
+  public:
+    FailedSocketError(int errno)
+        : message_("Socket failed: " + strerror(errno)) {
+    }
+
+    virtual const std::string what() const noexcept override {
+      return message_;
+    }
+}
+
+}  // namespace phi
 
 #endif /* EXCEPTIONS_HPP */
