35084bae830537c4a1fec922253a6470866eb8a5
zeviberlin <zeviberlin@gmail.com>
2026-01-01 20:01:25 -0500

    Colors

--- include/phi/ui/color_defs.hpp
+++ include/phi/ui/color_defs.hpp
@@ -3,4 +3,37 @@
 
 #include <ftxui/screen/color.hpp>
 
+namespace phi::ui::colors {
+
+const auto RED_MAROON = ftxui::Color::RGB(115, 20, 10);
+const auto RED = ftxui::Color::RGB(220, 30, 20);
+
+const auto ORANGE_BURNT = ftxui::Color::RGB(210, 60, 10);
+const auto ORANGE_CREAM = ftxui::Color::RGB(255, 170, 115);
+const auto ORANGE = ftxui::Color::RGB(240, 90, 10);
+
+const auto GOLD = ftxui::Color::RGB(210, 140, 15);
+const auto YELLOW_BANANA = ftxui::Color::RGB(255, 225, 50);
+const auto YELLOW_LIGHT = ftxui::Color::RGB(245, 238, 144);
+const auto BROWN = ftxui::Color::RGB(130, 85, 5);
+
+const auto GREEN_WIZARD = ftxui::Color::RGB(20, 105, 5);
+const auto GREEN = ftxui::Color::RGB(20, 190, 70);
+const auto GREEN_MINT = ftxui::Color::RGB(160, 250, 150);
+
+const auto TEAL = ftxui::Color::RGB(170, 250, 210);
+const auto BLUE_NAVY = ftxui::Color::RGB(10, 50, 120);
+const auto BLUE = ftxui::Color::RGB(20, 100, 200);
+const auto BLUE_BABY = ftxui::Color::RGB(140, 210, 240);
+
+const auto PURPLE_HAZE = ftxui::Color::RGB(60, 5, 100);
+const auto PURPLE_ROYAL = ftxui::Color::RGB(70, 20, 150);
+const auto PURPLE = ftxui::Color::RGB(100, 20, 200);
+const auto PURPLE_LIGHT = ftxui::Color::RGB(170, 115, 230);
+
+const auto MAGENTA = ftxui::Color::RGB(235, 30, 230);
+const auto PINK = ftxui::Color::RGB(250, 140, 200);
+
+}  // namespace phi::ui::colors
+
 #endif /* _COLOR_DEFS_HPP */
